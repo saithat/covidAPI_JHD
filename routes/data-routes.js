3c69e9c78750e5cedf3a795f132687320851add4
saithat <sai.thatigotla@gmail.com>
2020-07-24 09:03:53 -0400

    case insensitive query

--- routes/data-routes.js
+++ routes/data-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Data = require('../models/Data');
+const { query } = require('express');
 
 // Re-route to /api to show the data
 router.get('/', async (req, res) => {
@@ -20,11 +21,14 @@ router.get('/api', async (req, res) => {
 // Shows data of the given location_id
 router.get('/api/:id', async (req, res) => {
   try {
-    const covidData = await Data.find({ Province_State: req.params.id });
+    var q = req.params.id.toLowerCase();
+    q = q.charAt(0).toUpperCase() + q.slice(1);
+    const covidData = await Data.find({ Province_State: q});
+    //const covidData = await Data.find({ Province_State: new RegExp('/^' +  req.params.id + '/' )});
     res.json(covidData);
   } catch (err) {
     res.json({ message: err });
   }
-});
+}, () => console.log(red.params.id));
 
 module.exports = router;
